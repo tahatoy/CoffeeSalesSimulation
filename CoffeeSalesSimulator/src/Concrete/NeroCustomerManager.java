6bc636e0848c5d255566631885c3118dd200c9b0
tahatoyy <mttoy45@gmail.com>
2021-05-04 22:31:21 +0300

    Commit for oguzhan

--- CoffeeSalesSimulator/src/Concrete/NeroCustomerManager.java
+++ CoffeeSalesSimulator/src/Concrete/NeroCustomerManager.java
@@ -7,19 +7,19 @@ public class NeroCustomerManager implements CustomerService{
 	
 	@Override
 	public void add(Customer customer) {
-		System.out.println("Kullanýcý sisteme eklendi: "+customer.getFirstName()+" "+customer.getLastName());
+		System.out.println("Kullanici sisteme eklendi: "+customer.getFirstName()+" "+customer.getLastName());
 		
 	}
 
 	@Override
 	public void update(Customer customer) {
-		System.out.println("Kullanýcý güncellendi: "+customer.getFirstName()+" "+customer.getLastName());
+		System.out.println("Kullanici guncellendi: "+customer.getFirstName()+" "+customer.getLastName());
 		
 	}
 
 	@Override
 	public void delete(Customer customer) {
-		System.out.println("Kullanýcý silindi: "+customer.getFirstName()+" "+customer.getLastName());
+		System.out.println("Kullanici silindi: "+customer.getFirstName()+" "+customer.getLastName());
 		
 	}
 
--- CoffeeSalesSimulator/src/Concrete/StarbucksCustomerManager.java
+++ CoffeeSalesSimulator/src/Concrete/StarbucksCustomerManager.java
@@ -13,26 +13,26 @@ public class StarbucksCustomerManager implements CustomerService {
 	@Override
 	public void add(Customer customer) {
 		if(customerValidatorService.validate(customer)==true) {
-			System.out.println("Kullanýcý sisteme eklendi: "+customer.getFirstName()+" "+customer.getLastName());
+			System.out.println("Kullanici sisteme eklendi: "+customer.getFirstName()+" "+customer.getLastName());
 		}
 		else
-			System.out.println("Doðrulama hatasý oluþtu!!");
+			System.out.println("Dogrulama hatasi olustu!!");
 		
 	}
 
 	@Override
 	public void update(Customer customer) {
-		System.out.println("Kullanýcý güncellendi: "+customer.getFirstName()+" "+customer.getLastName());
+		System.out.println("Kullanici guncellendi: "+customer.getFirstName()+" "+customer.getLastName());
 		
 	}
 
 	@Override
 	public void delete(Customer customer) {
-		System.out.println("Kullanýcý silindi: "+customer.getFirstName()+" "+customer.getLastName());
+		System.out.println("Kullanici silindi: "+customer.getFirstName()+" "+customer.getLastName());
 		
 	}
 	public void addCoffee(int starPoint) {
-	     System.out.println("Hesabýnýza "+starPoint+" yýldýz eklenmiþtir.");
+	     System.out.println("Hesabiniza "+starPoint+" yildiz eklenmistir.");
 	
 	}
 }
